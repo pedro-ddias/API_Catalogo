10dd8453d4040cd3295ce019303a25d1e66a8278
Pedro Henrique Damasceno Dias <pedro.damasceno.dias@avanade.com>
2025-09-25 18:51:55 -0300

    Classe de contexto

---
+++ API_Catalogo/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using API_Catalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Catalogo.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) //configuração de string de conexão
+    {
+            
+    }
+    //? = para definir que será opcional
+    public DbSet<Categoria>? Categorias { get; set; }
+    public DbSet<Produto>? Produtos { get; set; }
+}
